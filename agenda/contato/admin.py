1fffe30203c41a447412926742d4a944b85d09ad
Felipe Reis <felipe.s.reis@live.com>
2017-04-25 16:27:19 -0300

    execução do comando para criação do banco de dados e teste da tela administrador

--- agenda/contato/admin.py
+++ agenda/contato/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from agenda.contato.models import contato
 
+
 class contatoAdmin(admin.ModelAdmin):
-    model: contato
-    list_display = ['contato_nome', 'contato_email', 'contato_favorito']
+
+    model = contato
+    list_display = ['contato_nome', 'contato_email', 'contato_estado_civil', 'contato_favorito']
     list_filter = ['contato_sexo', 'contato_estado_civil']
     search_fields = ['contato_nome']
     save_on_top = True
 admin.site.register(contato, contatoAdmin)
-
