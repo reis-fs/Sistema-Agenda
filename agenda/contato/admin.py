da7a170ffce7cdee5fc019060a9c69e160d1b987
Felipe Reis <felipe.s.reis@live.com>
2017-04-25 05:37:53 -0300

    Correção do erro através de atualização do python 3.5 para 3.6.1

--- agenda/contato/admin.py
+++ agenda/contato/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from agenda.contato.models import contato
 
 class contatoAdmin(admin.ModelAdmin):
-    model:contato
+    model: contato
     list_display = ['contato_nome', 'contato_email', 'contato_favorito']
     list_filter = ['contato_sexo', 'contato_estado_civil']
     search_fields = ['contato_nome']
