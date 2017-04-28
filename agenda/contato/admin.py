7c051be9a8e1447a78f3e4e5967bddfa9cd8df6e
Felipe Reis <felipe.s.reis@live.com>
2017-04-28 20:54:16 -0300

    Correções nos nomes das que foram alteradas.

--- agenda/contato/admin.py
+++ agenda/contato/admin.py
@@ -1,31 +1,31 @@
 from django.contrib import admin
-from agenda.contato.models import contato, tarefa, conta
+from agenda.contato.models import Contato, Tarefa, Conta
 
 
 class ContatoAdmin(admin.ModelAdmin):
 
-    model = contato
+    model = Contato
     list_display = ['contato_nome', 'contato_email', 'contato_estado_civil', 'contato_favorito']
     list_filter = ['contato_sexo', 'contato_estado_civil']
     search_fields = ['contato_nome']
     save_on_top = True
-admin.site.register(contato, ContatoAdmin)
+admin.site.register(Contato, ContatoAdmin)
 
 
 class TarefaAdmin(admin.ModelAdmin):
 
-    model = tarefa
+    model = Tarefa
     list_display = ['tarefa_nome', 'tarefa_data_inicio', 'concluido']
     search_fields = ['tarefa_nome']
     save_on_top = True
-admin.site.register(tarefa, TarefaAdmin)
+admin.site.register(Tarefa, TarefaAdmin)
 
 
 class ContaAdmin(admin.ModelAdmin):
 
-    model = conta
+    model = Conta
     list_display = ['conta_nome', 'conta_data_vencimento', 'pago']
     search_fields = ['conta_nome']
     list_filter = ['pago']
     save_on_top = True
-admin.site.register(conta, ContaAdmin)
+admin.site.register(Conta, ContaAdmin)
