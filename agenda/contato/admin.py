1e13ab3d1884c359b3fb6a1b0af1bac35abc9fb4
Felipe Reis <felipe.s.reis@live.com>
2017-04-28 20:42:51 -0300

    Alteração no nome das classes por padronização de boas práticas.

--- agenda/contato/admin.py
+++ agenda/contato/admin.py
@@ -2,30 +2,30 @@ from django.contrib import admin
 from agenda.contato.models import contato, tarefa, conta
 
 
-class contatoAdmin(admin.ModelAdmin):
+class ContatoAdmin(admin.ModelAdmin):
 
     model = contato
     list_display = ['contato_nome', 'contato_email', 'contato_estado_civil', 'contato_favorito']
     list_filter = ['contato_sexo', 'contato_estado_civil']
     search_fields = ['contato_nome']
     save_on_top = True
-admin.site.register(contato, contatoAdmin)
+admin.site.register(contato, ContatoAdmin)
 
 
-class tarefaAdmin(admin.ModelAdmin):
+class TarefaAdmin(admin.ModelAdmin):
 
     model = tarefa
     list_display = ['tarefa_nome', 'tarefa_data_inicio', 'concluido']
     search_fields = ['tarefa_nome']
     save_on_top = True
-admin.site.register(tarefa, tarefaAdmin)
+admin.site.register(tarefa, TarefaAdmin)
 
 
-class contaAdmin(admin.ModelAdmin):
+class ContaAdmin(admin.ModelAdmin):
 
     model = conta
     list_display = ['conta_nome', 'conta_data_vencimento', 'pago']
     search_fields = ['conta_nome']
     list_filter = ['pago']
     save_on_top = True
-admin.site.register(conta, contaAdmin)
+admin.site.register(conta, ContaAdmin)
